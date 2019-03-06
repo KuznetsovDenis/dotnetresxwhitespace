4916bc1a80da617e10898ae228543675818a067e
Denis Kuznetsov <denis.kuznetsov@devexpress.com>
2019-03-06 16:57:56 +0300

    Init

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace dotnetresxwhitespace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string whitespaceFromResource = Resource1.ResourceManager.GetString("WhitespaceResource");
+            if(whitespaceFromResource == " ")
+                Console.WriteLine("The whitespace in resouces is correct");
+            else
+                Console.WriteLine("The whitespace in resources has been replaced with something else. Probably with the String.Empty");
+        }
+    }
+}
